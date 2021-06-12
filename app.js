c0e5403a7d009824c10640afb20a807c4f8bfac6
razahamza0308 <65039022+razahamza0308@users.noreply.github.com>
2021-06-12 09:49:02 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+	function calcNumbers(result){
+		form.displayResult.value +=result;
+		
+	}
+
+	function del(result){
+		form.displayResult.value=result;
+		
+	}
+
+	function cl(){
+		form.displayResult.value =form.displayResult.value.slice(0, - 1);
+	}
+
+
